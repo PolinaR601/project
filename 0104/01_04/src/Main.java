d50942ce4b54461cbd37096798451f26a021d027
tihon <presetova5@gmail.com>
2025-06-04 00:23:04 +0300

    pupu

--- 0104/01_04/src/Main.java
+++ 0104/01_04/src/Main.java
@@ -16,7 +16,7 @@ public class Main {
                             "3 - из 10 в 16 \n" +
                             "4 - из 16 в 10\n" +
                             "5 - из 2 в 16\n" +
-                            "6 - из 16 в 2\n");
+                            "6 - из 16 в 2");
                     deistv = in.nextInt();
                     System.out.print("введите число:");
                     chislo1 = in.next();
@@ -53,14 +53,16 @@ public class Main {
                     chislo2 = in.next();
                     switch (deistv) {
                         case 1:
-                            System.out.println(SlozhenieVdv(chislo1,chislo2));
+                            System.out.println(SlozhenieVdv(chislo1, chislo2));
                             break;
                         case 2:
+                            System.out.println(SlozhenieVShes(chislo1, chislo2));
                             break;
                         case 3:
-                            System.out.println(VichitanieVdv(chislo1,chislo2));
+                            System.out.println(VichitanieVdv(chislo1, chislo2));
                             break;
                         case 4:
+                            System.out.println(VichitanieVShes(chislo1, chislo2));
                             break;
                     }
                     break;
@@ -192,16 +194,121 @@ public class Main {
         int desyat = 0;
         int p = 0;
         for (int i = d.length() - 1; i >= 0; i--) {
-            char c = d.charAt(i);
             int s;
-            if (c >= '0' && c <= '9') {
-                s = c - '0';
+            if (d.charAt(i) >= '0' && d.charAt(i) <= '9') {
+                s = d.charAt(i) - '0';
             } else {
-                s = c - 'A' + 10;
+                s = d.charAt(i) - 'A' + 10;
             }
             desyat += s * Math.pow(16, p);
             p++;
         }
         return Integer.toString(desyat);
+    }
+
+    private static String VichitanieVShes(String chislo, String chislo2) {
+        char c, c2;
+        while (chislo.length() < chislo2.length()) {
+            chislo = "0" + chislo;
+        }
+        while (chislo2.length() < chislo.length()) {
+            chislo2 = "0" + chislo2;
+        }
+        String num = "";
+        StringBuilder res = new StringBuilder();
+        int s3 = 0;
+        for (int i = chislo.length() - 1; i >= 0; i--) {
+            c = chislo.charAt(i);
+            c2 = chislo2.charAt(i);
+            int s, s2;
+            if (c >= '0' && c <= '9')
+                s = c - '0';
+            else s = c - 'A' + 10;
+            if (c2 >= '0' && c2 <= '9')
+                s2 = c2 - '0';
+            else s2 = c2 - 'A' + 10;
+            num = String.valueOf(s - s2 - s3);
+            if (s - s2 - s3 < 0) {
+                num = String.valueOf(s - s2 - s3 + 16);
+                s3 = 1;
+            } else {
+                s3 = 0;
+            }
+            num = switch (num) {
+                case "1" -> "1";
+                case "2" -> "2";
+                case "3" -> "3";
+                case "4" -> "4";
+                case "5" -> "5";
+                case "6" -> "6";
+                case "7" -> "7";
+                case "8" -> "8";
+                case "9" -> "9";
+                case "10" -> "A";
+                case "11" -> "B";
+                case "12" -> "C";
+                case "13" -> "D";
+                case "14" -> "E";
+                case "15" -> "F";
+                default -> num;
+            };
+            res.append(num);
+        }
+        return res.reverse().toString();
+    }
+
+    private static String SlozhenieVShes(String chislo, String chislo2) {
+        char c = 0, c2 = 0;
+        chislo = "0" + chislo;
+        chislo2 = "0" + chislo2;
+        while (chislo.length() < chislo2.length()) {
+            chislo = "0" + chislo;
+        }
+        while (chislo2.length() < chislo.length()) {
+            chislo2 = "0" + chislo2;
+        }
+
+        String num;
+        StringBuilder res = new StringBuilder();
+        int s3 = 0;
+        for (int i = chislo.length() - 1; i >= 0; i--) {
+            c = chislo.charAt(i);
+            c2 = chislo2.charAt(i);
+            int s, s2;
+            if (c >= '0' && c <= '9')
+                s = c - '0';
+            else s = c - 'A' + 10;
+            if (c2 >= '0' && c2 <= '9')
+                s2 = c2 - '0';
+            else s2 = c2 - 'A' + 10;
+
+            num = String.valueOf(s + s2 + s3);
+            if (s + s2 + s3 > 15) {
+                num = String.valueOf(s + s2 + s3 - 16);
+                s3 = 1;
+            } else s3 = 0;
+            num = switch (num) {
+                case "1" -> "1";
+                case "2" -> "2";
+                case "3" -> "3";
+                case "4" -> "4";
+                case "5" -> "5";
+                case "6" -> "6";
+                case "7" -> "7";
+                case "8" -> "8";
+                case "9" -> "9";
+                case "10" -> "A";
+                case "11" -> "B";
+                case "12" -> "C";
+                case "13" -> "D";
+                case "14" -> "E";
+                case "15" -> "F";
+                default -> num;
+            };
+            res.append(num);
+        }
+        if (res.charAt(res.length() - 1) == '0') {
+            return res.reverse().substring(1).toString();
+        } else return res.reverse().toString();
     }
 }
